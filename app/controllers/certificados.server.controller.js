0d5d87dbd0076d90bbca66bdff5633f0cbe17d22
Paulo Machado <pviniciusfm@gmail.com>
2015-03-23 23:17:17 -0300

    Implementation of CSV parser and ZIP output

--- app/controllers/certificados.server.controller.js
+++ app/controllers/certificados.server.controller.js
@@ -3,60 +3,97 @@
 /**
  * Module dependencies.
  */
-var mongoose = require('mongoose'),
-    phantom = require('phantom'),
-    temp = require('temp'),
-    swig = require('swig'),
-    errorHandler = require('./errors.server.controller'),
-    Certificado = mongoose.model('Certificado'),
-    _ = require('lodash');
+var mongoose = require('mongoose');
+var phantom = require('phantom');
+var temp = require('temp');
+var swig = require('swig');
+var errorHandler = require('./errors.server.controller');
+var Certificado = mongoose.model('Certificado');
+var Converter = require('csvtojson').core.Converter;
+var Archiver = require('archiver');
+var fs = require('fs');
+var _ = require('lodash');
 
 /**
  * Generate Certificado
  */
 exports.create = function(req, res) {
     var certificado = new Certificado(req.body);
-    var tempName = temp.path({
-        suffix: '.pdf'
+    var fileStream = fs.createReadStream('teste.csv');
+    var csvConverter = new Converter({});
+    var archive = new Archiver('zip');
+    var outputFileName = temp.path({
+        suffix: '.zip'
     });
+    var output = fs.createWriteStream(outputFileName);
 
-    phantom.create(function(ph) {
-        ph.createPage(function(page) {
-            page.settings = {
-                loadImages: true,
-                localToRemoteUrlAccessEnabled: true,
-                javascriptEnabled: true,
-                loadPlugins: false
-            };
+    csvConverter.on('end_parsed', function(jsonObj) {
 
-            page.set('paperSize', {
-                format: 'A4',
-                orientation: 'portrait',
-                border: '1cm'
-            });
+        phantom.create(function(ph) {
+            ph.createPage(function(page) {
+                page.settings = {
+                    loadImages: true,
+                    localToRemoteUrlAccessEnabled: true,
+                    javascriptEnabled: true,
+                    loadPlugins: false
+                };
 
-            var html = swig.renderFile('public/modules/certificados/views/template-certificado.cliente.view.html', {
-                name: 'Participante 1',
-                eventName: 'GDG-BH Extended'
-            });
+                page.set('paperSize', {
+                    format: 'A4',
+                    orientation: 'portrait',
+                    border: '1cm'
+                });
 
-            page.setContent(html,'http://gdhbh.com' );
+                archive.pipe(output);
 
-            page.render(tempName, function(error) {
-                if (error) console.log('Error rendering PDF: %s', error);
-                ph.exit();
+                var pdfFiles = [];
+                _.each(jsonObj, function(participante) {
+                    var tempName = temp.path({
+                        suffix: '.pdf'
+                    });
+                    var html = swig.renderFile('public/modules/certificados/views/template-certificado.cliente.view.html', participante);
+                    page.setContent(html, 'http://gdhbh.com');
+                    page.render(tempName, function(error) {
+                        if (error) {
+                            console.log('Error rendering PDF: %s', error)
+                            ph.exit();
+                        }
+                    });
+                    archive.append(fs.createReadStream(tempName), {
+                        name: participante.nomeParticipante + '.pdf'
+                    });
+                });
+                archive.finalize();
             });
         });
+
+        output.on('close', function() {
+            var stripfileName = outputFileName.substring(outputFileName.lastIndexOf('/') + 1, outputFileName.length);
+            res.jsonp({
+                fileName: stripfileName
+            });
+        });
+
     });
 
-    var stripfileName = tempName.substring(tempName.lastIndexOf('/') + 1, tempName.length);
-    res.jsonp({
-        fileName: stripfileName
-    });
+    fileStream.pipe(csvConverter);
 };
 
 exports.download = function(req, res) {
     res.download('/tmp/' + req.params.downloadId);
+};
+
+exports.uploadCsv = function(req, res) {
+    var tempName = temp.path({
+        suffix: '.pdf'
+    });
+    fs.readFile(req.files.displayImage.path, function(err, data) {
+        fs.writeFile(tempName, data, function(err) {
+            res.jsonp({
+                fileName: tempName
+            });
+        });
+    });
 };
 
 /**
